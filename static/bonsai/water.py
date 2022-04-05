cfa2d5c59feda8e4df085154cb0e99ed4001499d
lukeorriss <lukeorriss@outlook.com>
2022-04-05 17:12:12 +0100

    Create water.py

---
+++ static/bonsai/water.py
@@ -0,0 +1,21 @@
+import time
+import math
+import os
+import RPi.GPIO as GPIO
+from datetime import datetime
+
+moisture = 22
+pump = 27
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(moisture, GPIO.IN)
+GPIO.setup(pump, GPIO.OUT)
+
+
+
+while True: 
+    GPIO.output(pump, GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(pump, GPIO.LOW)
+    time.sleep(5)
