869fdc12363e5779ba639734b8534fca2da91551
lukeorriss <lukeorriss@outlook.com>
2022-04-07 22:51:44 +0100

    Update simpletest.py

--- static/bonsai/simpletest.py
+++ static/bonsai/simpletest.py
@@ -18,9 +18,14 @@ print('Reading ADS1x15 values, press Ctrl-C to quit...')
 print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
 print('-' * 37)
 
+# Completely Wet ~ 10700
+# Completely Dry ~ 14350
+
+
+
 while True:
     values = [0]*4
     for i in range(4):
         values[i] = adc.read_adc(i, gain=GAIN)
-    print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+    print('{0:>6}'.format(*values))
     time.sleep(2)
